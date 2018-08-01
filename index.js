a35d1a1d4de1d647c39c7f14b4edb65380ad3d41
Matt Trussell <smtrussell@gmail.com>
2018-08-01 17:59:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var customerName = 'bob';
 // const leastFavoriteCustomer = 'sour fred'
 //
 function upperCaseCustomerName() {
-  return customerName = customerName.toUpperCase()
+  return customerName = customerName.toUpperCase();
 }
 //
 // function changeLeastFavoriteCustomer(){
