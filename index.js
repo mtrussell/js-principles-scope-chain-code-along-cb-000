93a4b85db31a9d1e1a56093dbcfb57f19143e9c3
Matt Trussell <smtrussell@gmail.com>
2018-08-01 18:02:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function overWriteBestCustomer(newFavorite){
   bestCustomer = newFavorite;
 }
 
-// function congratulateCustomer(){
-//   return `congrats ${favoriteCustomer}`
-// }
-//
+function congratulateCustomer(){
+  return `congrats ${favoriteCustomer}`
+}
+
 // function attemptTwoFavoriteCustomers(){
 //     let favoriteCustomer = 'bar'
 //     let favoriteCustomer = 'not bar'
